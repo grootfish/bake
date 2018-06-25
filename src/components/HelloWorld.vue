07283dd07d546e99645d6f4bcfd2bc5052e7e189
grootfish <groot01@163.com>
2018-06-25 09:07:02 +0800

    second commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import { Button, Select, Container, Aside, Header, Main } from 'element-ui';
+
+
+Vue.use(Button);
+Vue.use(Select);
+Vue.use(Container);
+Vue.use(Aside);
+Vue.use(Header);
+Vue.use(Main);
+
+
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: 'This is our Life',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+</style>
